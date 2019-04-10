11fefcce833d3c296d6c2b5fe4cc522fb9e47d90
chenchun <goodMorning@163.com>
2019-04-10 22:28:42 +0800

    22222

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     @RequestMapping({"/","/index.html"})
     public String index(){
-        System.out.println("12334");
+        System.out.println("12334355");
         return "index";
     }
 }
